b950804fa6c9e5526d9d2311f890f04925756018
hnhai <hnhai.dev@gmail.com>
2021-05-20 22:53:53 +0700

    add App.js, update Layout and Navbar

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -2,15 +2,22 @@ import React from "react";
 import HeadTags from "./HeadTags";
 import Navbar from "./Navbar";
 import { Container } from "semantic-ui-react";
+import nprogress from "nprogress";
+import Router from "next/router";
 
-function Layout() {
+function Layout({ children }) {
+  Router.onRouteChangeStart = () => nprogress.start();
+  Router.onRouteChangeComplete = () => nprogress.done();
+  Router.onRouteChangeError = () => nprogress.done();
+
   return (
     <>
       <HeadTags />
-
       <Navbar />
 
-      <Container style={{ paddingTop: "1rem" }} text></Container>
+      <Container text style={{ paddingTop: "1rem" }}>
+        {children}
+      </Container>
     </>
   );
 }
