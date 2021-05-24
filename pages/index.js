f27acb38af34b2b73b2f9e383fcf7c621b556b40
hnhai <hnhai.dev@gmail.com>
2021-05-24 23:53:41 +0700

    Creating HomePage Grid

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 import baseUrl from "../utils/baseUrl";
 
 function Index({ user, userFollowStats }) {
-  console.log({ user, userFollowStats });
+  useEffect(() => {
+    document.title = `Welcome, ${user.name.split(" ")[0]}`;
+  }, []);
   return <div>Page</div>;
 }
 
