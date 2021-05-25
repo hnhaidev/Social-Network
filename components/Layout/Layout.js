5f719e56a5c44a56c5880f236e7517effc499eb9
hnhai <hnhai.dev@gmail.com>
2021-05-25 20:20:15 +0700

    Search for user

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -17,7 +17,7 @@ import Search from "./Search";
 
 function Layout({ children, user }) {
   const contextRef = createRef();
-  // Tao thanh tien trinh voi nprogress
+
   Router.onRouteChangeStart = () => nprogress.start();
   Router.onRouteChangeComplete = () => nprogress.done();
   Router.onRouteChangeError = () => nprogress.done();
@@ -26,34 +26,31 @@ function Layout({ children, user }) {
     <>
       <HeadTags />
       {user ? (
-        <>
-          <div style={{ marginLeft: "1rem", marginRight: "1rem" }}>
-            <Ref innerRef={contextRef}>
-              <Grid>
-                <Grid.Column floated="left" width={2}>
-                  <Sticky context={contextRef}>
-                    <SideMenu user={user} />
-                  </Sticky>
-                </Grid.Column>
+        <div style={{ marginLeft: "1rem", marginRight: "1rem" }}>
+          <Ref innerRef={contextRef}>
+            <Grid>
+              <Grid.Column floated="left" width={2}>
+                <Sticky context={contextRef}>
+                  <SideMenu user={user} />
+                </Sticky>
+              </Grid.Column>
 
-                <Grid.Column width={10}>
-                  <Visibility context={contextRef}>{children}</Visibility>
-                </Grid.Column>
+              <Grid.Column width={10}>
+                <Visibility context={contextRef}>{children}</Visibility>
+              </Grid.Column>
 
-                <Grid.Column floated="left" width={4}>
-                  <Sticky context={contextRef}>
-                    <Segment basic>
-                      <Search />
-                    </Segment>
-                  </Sticky>
-                </Grid.Column>
-              </Grid>
-            </Ref>
-          </div>
-        </>
+              <Grid.Column floated="left" width={4}>
+                <Sticky context={contextRef}>
+                  <Segment basic>
+                    <Search />
+                  </Segment>
+                </Sticky>
+              </Grid.Column>
+            </Grid>
+          </Ref>
+        </div>
       ) : (
         <>
-          {" "}
           <Navbar />
           <Container text style={{ paddingTop: "1rem" }}>
             {children}
