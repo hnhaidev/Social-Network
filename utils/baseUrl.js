b8949212f80f50318ec2eb8d4496aaad37039d67
hnhaidev <hnhai.dev@gmail.com>
2021-06-18 22:30:18 +0700

    test project vercel

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,5 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://utc2-network.herokuapp.com";
+    : "https://utc2-network.herokuapp.com" || "https://utc2-network.vercel.app";
 export default baseUrl;
